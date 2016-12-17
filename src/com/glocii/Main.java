6df8a446171123670ba74917170461849589a7c4
Vladislav Belikov <Vladislav Belikov>
2016-12-18 01:12:35 +0300

    Hello Sancho

--- src/com/glocii/Main.java
+++ src/com/glocii/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.print("Hello, Vlad! \nPlease, answer me!");
+	    System.out.println("Hello Sancho!\n I'm answer you!");
     }
 }
