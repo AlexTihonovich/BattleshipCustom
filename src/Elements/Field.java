0159995799f40983f366428dc4d975fc46f780b4
Vladislav Belikov <Vladislav Belikov>
2016-12-27 01:24:43 +0300

    costruct cells

--- src/Elements/Field.java
+++ src/Elements/Field.java
@@ -1,5 +1,23 @@
 package Elements;
 
-public class Field {
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Field extends JPanel{
+	private final int SIZE = 10;
+	private Cell[][] cell;
+	public Field(){
+		super();
+		cell = new Cell[SIZE][SIZE];
+		for(int i = 0; i < SIZE; i++)
+			for(int j = 0; j < SIZE; j++)
+				cell[i][j] = new Cell(20);
+	}
+	@Override
+	public void paint(Graphics g){
+		
+	}
+	
 
 }
