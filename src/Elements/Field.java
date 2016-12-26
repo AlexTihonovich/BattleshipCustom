b5791b444d8d1050c30a3232b96b72221de48030
Vladislav Belikov <Vladislav Belikov>
2016-12-27 01:56:50 +0300

    draw string

--- src/Elements/Field.java
+++ src/Elements/Field.java
@@ -1,12 +1,16 @@
 package Elements;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
 public class Field extends JPanel{
 	private final int SIZE = 10;
+	private Font font;
+	private final String al = "АБВГДЕЖЗИК";
 	private Cell[][] cell;
 	public Field(){
 		super();
@@ -19,19 +23,33 @@ public class Field extends JPanel{
 				cell[i][j].setY(cell[i][j].getSize()*j+cell[i][j].getSize()/2);
 				
 			}
-		Cell oneCell = cell[0][0];
+		Cell firstCell = cell[0][0];
+		font = new Font("Verdana", Font.PLAIN, firstCell.getSize()/2);
 		//Задаём размеры поля
-		this.setPreferredSize(new Dimension(oneCell.getSize()*SIZE+oneCell.getSize(),
-				oneCell.getSize()*SIZE+oneCell.getSize()));
+		this.setPreferredSize(new Dimension(firstCell.getSize()*SIZE+firstCell.getSize()*2,
+				firstCell.getSize()*SIZE+firstCell.getSize()*2));
 		this.setDoubleBuffered(true);
 	
 	}
 	@Override
 	public void paint(Graphics g){
+		Cell firstCell = cell[0][0];
+		int x = firstCell.getX() - firstCell.getSize()/2;
+		int y = firstCell.getY() - firstCell.getSize()/2;
 		for(int i = 0; i < SIZE; i++)
 			for(int j = 0; j < SIZE; j++){
 				cell[i][j].paint(g);
 			}
+		g.setColor(Color.black);
+		g.setFont(font);
+		for(int i = 0; i < SIZE; i++){
+			g.drawString(String.valueOf(al.charAt(i)), x, firstCell.getY() - firstCell.getSize()/2);
+			g.drawString(String.valueOf(i+1), y, firstCell.getX() - firstCell.getSize()/2);
+			System.out.println(x+" "+y);
+			x+= firstCell.getSize();
+			y+= firstCell.getSize();
+			
+		}
 	}
 	
 
